5dccac28572f598eeaa66e22b3aa49d1b2d1dc1b
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-15 01:07:32 -0700

    chore: fix dest

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,4 @@ import { defineConfig } from "nitro";
 export default defineConfig({
   preset: "netlify",
   serverEntry: "./src/server.ts",
-  output: {
-    dir: "dist",
-  },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
           src: fileURLToPath(
             import.meta.resolve("libexif-wasm/output/libexif.wasm"),
           ),
-          dest: "server/_libs",
+          dest: "../.netlify/functions-internal/server/_libs",
           rename: { stripBase: true },
         },
       ],
